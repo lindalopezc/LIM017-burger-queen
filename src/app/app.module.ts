464b02d25da2b2123f1f55f2cbe18406ebe3fa05
JaquelineRocio <jaquelineramosvargas@gmail.com>
2022-06-27 20:32:32 -0500

    fixing problems

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -98,7 +98,7 @@ import {MatFormFieldModule} from '@angular/material/form-field';
       enabled: environment.production,
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
+      registrationStrategy: 'registrationStrategy'
     }),
     provideStorage(()=>getStorage())
   ],
