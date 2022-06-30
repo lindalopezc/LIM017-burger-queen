5bf64ec239263469b37c293870a0446c446601eb
lindalopezc <lindalopezramos61@gmail.com>
2022-06-30 09:54:18 -0500

    update admi component

--- src/app/waiter/body/menu/menu.component.ts
+++ src/app/waiter/body/menu/menu.component.ts
@@ -12,9 +12,17 @@ export class MenuComponent implements OnInit {
 
   ngOnInit(): void {
     this.productFirebase.getProducts().subscribe((products)=>{
-      this.menu['burguers'].push(...products.filter(product=>product.typeProduct === 'Burger'));
-      this.menu['beverages'].push(...products.filter(product=>product.typeProduct === 'Beverages'))
+      this.menu['burguers'].push(...products.filter(product=>product.typeProduct === 'Burger'))
+      this.menu['burguers'] = [...new Set(this.menu['burguers'])];
+      this.menu['beverages'].push(...products.filter(product=>product.typeProduct === 'Beverages'));
       this.menu['accompaniments'].push(...products.filter(product=>product.typeProduct === 'Accompaniments'))
+      // this.menu['accompaniments'] = [...new Set(this.menu['accompaniments'])];
+
+      this.menu['accompaniments'] = this.menu['accompaniments'].filter((item: any,index: number)=>{
+        return this.menu['accompaniments'].indexOf(item) === index;
+      })
+
+      console.log(this.menu['accompaniments'])
     })
   }
 }
